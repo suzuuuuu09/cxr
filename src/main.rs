fa86c47796105e1e7d364214cddc76b4c5f514b3
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-05-28 15:09:48 +0900

    ✨ xDG_CONFIG_HOMEにテンプレートを保存し使えるようにした

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, Write};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 mod args;
 mod generator;
@@ -12,6 +12,14 @@ use args::{Cli, Commands};
 use template::Template;
 
 const DEFAULT_YAML: &str = include_str!("./default.yaml");
+
+// XDG_CONFIG_HOME環境変数を使用して設定ディレクトリを取得する関数
+fn get_config_dir() -> Option<PathBuf> {
+    match std::env::var("XDG_CONFIG_HOME") {
+        Ok(val) => Some(Path::new(&val).join("cx")),
+        Err(_) => dirs::home_dir().map(|path| path.join(".config").join("cx")),
+    }
+}
 
 fn main() {
     // コマンドライン引数を解析する
@@ -27,18 +35,29 @@ fn main() {
                 }
             }
             Commands::New { name } => {
+                let config_dir = match get_config_dir() {
+                    Some(path) => path,
+                    None => {
+                        eprintln!("Failed to find configuration directory.");
+                        return;
+                    }
+                };
+
+                // ディレクトリが存在しない場合は作成する
+                std::fs::create_dir_all(&config_dir).unwrap();
+
                 // 新しいテンプレートファイルを作成する
-                let filename = format!("{}.yaml", name);
+                let filename = config_dir.join(format!("{}.yaml", name));
                 match File::create(&filename) {
                     Ok(mut file) => {
                         use std::io::Write;
                         if let Err(e) = file.write_all(DEFAULT_YAML.as_bytes()) {
-                            eprintln!("Failed to write to template file '{}': {}", filename, e);
+                            eprintln!("Failed to write to template file '{:?}': {}", filename, e);
                         } else {
-                            println!("Template file '{}' created successfully.", filename);
+                            println!("Template file '{:?}' created successfully.", filename);
                         }
                     }
-                    Err(e) => eprintln!("Failed to create template file '{}': {}", filename, e),
+                    Err(e) => eprintln!("Failed to create template file '{:?}': {}", filename, e),
                 }
             }
             Commands::List => {
@@ -50,8 +69,16 @@ fn main() {
     }
 
     if let Some(template_arg) = cli.template {
-        let filename = format!("{}.yaml", template_arg);
-        match std::fs::read_to_string(&filename) {
+        let config_dir = match get_config_dir() {
+            Some(path) => path,
+            None => {
+                eprintln!("Failed to find configuration directory.");
+                return;
+            }
+        };
+
+        let template_file_path = config_dir.join(format!("{}.yaml", template_arg));
+        match std::fs::read_to_string(&template_file_path) {
             Ok(content) => {
                 println!("{}", content);
                 match serde_yaml::from_str::<Template>(&content) {
@@ -89,10 +116,10 @@ fn main() {
                         println!("Items:");
                         generator::create_items(&template.items, Path::new("."), &variable_map);
                     }
-                    Err(e) => eprintln!("Failed to parse template file '{}': {}", filename, e),
+                    Err(e) => eprintln!("Failed to parse template file '{:?}': {}", config_dir, e),
                 }
             }
-            Err(e) => eprintln!("Failed to read template file '{}': {}", filename, e),
+            Err(e) => eprintln!("Failed to read template file '{:?}': {}", config_dir, e),
         }
     }
 }
